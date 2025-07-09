510f69c2b0d113d1f19f90eb5f1af36c89f21f91
Camilo Cunha de Azevedo <camilotk@gmail.com>
2025-07-09 17:02:09 -0300

    Update example.erl

--- safety-examples/spatial/buffer-safety/example.erl
+++ safety-examples/spatial/buffer-safety/example.erl
@@ -1,6 +1,7 @@
 -module(example).
 -export([handle_string/1]).
 
-handle_string(InputString) ->
-    Buffer = InputString,
-    io:format("Buffer content: ~s~n", [Buffer]).
+handle_string(InputString) when is_list(InputString) ->
+    io:format("Received string: ~ts~n", [InputString]);
+handle_string(InputBin) when is_binary(InputBin) ->
+    io:format("Received binary string: ~s~n", [InputBin]).
